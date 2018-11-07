650e37a45ad86848bf2485272318bb8e448d8427
LukevandeVenis <lukevandevenis@hotmail.com>
2018-11-07 15:17:53 +0100

    gebruikers kunnen gezien worden + gebruiker pagina

--- blog/resources/views/user.blade.php
+++ blog/resources/views/user.blade.php
@@ -1,4 +1,39 @@
 @extends('layouts.app')
 @section('content')
     <h2>Gebruikers tabel</h2>
+
+    <table class="table table-hover">
+
+        <thead>
+
+        <th>ID</th>
+
+        <th>Gebruikersnaam</th>
+
+        <th>Wachtwoord</th>
+
+        <th>E-mail</th>
+
+        </thead>
+
+        <tbody>
+        @foreach($users as $user)
+
+            <tr>
+
+                <td>{{$user->id}} </td>
+
+                <td>{{$user->name}} </td>
+
+                <td>{{$user->password}} </td>
+
+                <td>{{$user->email}} </td>
+
+
+            </tr>
+        @endforeach
+
+        </tbody>
+
+    </table>
 @stop
